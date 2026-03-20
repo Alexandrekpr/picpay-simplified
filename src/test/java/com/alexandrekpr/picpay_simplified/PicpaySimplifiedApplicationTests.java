7c4bc73ad708324f6e4607283f8c2ba767b92989
Alexandre <alexandrekpr@gmail.com>
2026-03-20 17:09:21 -0300

    feat: initial config

---
+++ src/test/java/com/alexandrekpr/picpay_simplified/PicpaySimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandrekpr.picpay_simplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
